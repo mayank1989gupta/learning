e396922f682ea0534744c9e0effd798ba677aa69
Mayank Gupta <Mayank@Mayanks-MacBook-Pro.local>
2018-07-08 01:05:47 +0200

    added new sample for Executor with Callable

---
+++ src/com/learning/samplers/ExecutorSample.java
@@ -0,0 +1,78 @@
+/**
+ * 
+ */
+package com.learning.samplers;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+/**
+ * @author Mayank
+ *
+ */
+public class ExecutorSample implements Callable<String> {
+	private int number;
+	public ExecutorSample(int number) {
+		this.number = number;
+	}
+
+	private static ExecutorService executor = Executors.newFixedThreadPool(5);
+
+	/**
+	 * @param args
+	 * @throws ExecutionException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+
+		Callable<String> callable = null;
+		List<Future<String>> list = new ArrayList<Future<String>>();
+		Future<String> future = null;
+
+		for(int i=10; i <= 15; i++){
+			callable = new ExecutorSample(i);
+			future = executor.submit(callable);
+			list.add(future);
+		}
+
+		/**
+		 * For 1&2 second task it will return immediately. 
+		 * We will wait for the third task will get completed. 
+		 * Even 4th task completed , iteration is waiting in third task . 
+		 * Once third task completed or timed wait expire on that time only iteration will continue.
+		 */
+		for(Future<String> fut : list) {
+			System.out.println("main()" + " **** " + Thread.currentThread().getName() + " **** " + fut.get());
+			//System.out.println(fut.get());
+		}
+
+		executor.shutdown();
+
+	}
+
+	@Override
+	public String call() throws Exception {
+		StringBuilder result = new StringBuilder();
+		result.append("For Number: ").append(number).append(": ");
+		for(int i = 0 ; i < number; i++) {
+			if(number == 12) {
+				Thread.sleep(500);
+			}
+			result.append("Number: ").append(i).append("  ");
+		}
+
+		return result.toString();
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		executor.shutdown();
+		super.finalize();
+	}
+
+}
--- src/com/learning/samplers/StacksBalancedBrackets.java
+++ src/com/learning/samplers/StacksBalancedBrackets.java
@@ -15,14 +15,16 @@ public class StacksBalancedBrackets {
 	private static final Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		int t = scanner.nextInt();
+		/*int t = scanner.nextInt();
 		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
 
 		for (int tItr = 0; tItr < t; tItr++) {
 			String expression = scanner.nextLine();
 			checkBalanceBrackets(expression);
-		}
+		}*/
 
+		
+		System.out.println(checkBalanceBrackets("{}("));
 		scanner.close();
 		
 		//System.out.println(checkBalanceBrackets("[()][{}()][](){}([{}(())([[{}]])][])[]([][])(){}{{}{[](){}}}()[]({})[{}{{}([{}][])}]"));
@@ -31,7 +33,7 @@ public class StacksBalancedBrackets {
 	private static String checkBalanceBrackets(String expression) {
 		int expressionLength = expression.length();
 		String result = "YES";
-		if(expressionLength == 0) {
+		if(expressionLength == 0 || expressionLength % 2 != 0) {
 			result = "NO";//For empty string or, odd length string
 		} else {
 			Stack<Character> stack = new Stack<>();
